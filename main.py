b4c84b5d6f0ace81348ca19927012d381eb97481
thiagoquilici <thiagoquilici@gmail.com>
2024-05-23 20:26:26 -0300

    Versão 1.0.

---
+++ main.py
@@ -0,0 +1,23 @@
+# lista
+nomes =  ['Alex', 'Simone', 'Bernardo', 'César', 'Alexandra']
+
+# exibe a lista na tela
+#for i in range(len(nomes)):
+#    print(f'{i+1}º nome da lista: {nomes[i]}.')
+
+# exibe a lista na tela
+#for nome in nomes:
+#   print(nomes)
+
+# exibe a lista na tela
+#    print(nomes)
+
+# ordenando a lista
+#nomes.sort()
+
+# ordenando a lista em ordem reversa
+nomes.sort(reverse=True)
+
+# exibe a lista na tela
+for nome in nomes:
+    print(nome)
